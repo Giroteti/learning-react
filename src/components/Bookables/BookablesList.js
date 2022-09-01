d9ca91d6ae7e780c18428eb668476ef111598595
Etienne Girot <etienne.girot@sundayapp.com>
2022-09-01 16:07:50 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,25 +1,32 @@
 import {useState} from 'react';
 
-export default function BookablesList ({bookables}) {
-  const group = "Rooms";
-  const bookablesInGroup = bookables.filter(b => b.group === group);
-  const [bookableIndex, setBookableIndex] = useState(1);
+export default function BookablesList({bookables}) {
+    const group = "Rooms";
+    const bookablesInGroup = bookables.filter(b => b.group === group);
+    const [bookableIndex, setBookableIndex] = useState(1);
 
-  return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((b, i) => (
-        <li
-          key={b.id}
-          className={i === bookableIndex ? "selected" : null}
-        >
-          <button
-            className="btn"
-            onClick={() => setBookableIndex(i)}
-          >
-            {b.title}
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <div>
+            <ul className="bookables items-list-nav">
+                {bookablesInGroup.map((b, i) => (
+                    <li
+                        key={b.id}
+                        className={i === bookableIndex ? "selected" : null}
+                    >
+                        <button
+                            className="btn"
+                            onClick={() => setBookableIndex(i)}
+                        >
+                            {b.title}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <button
+                className="btn"
+                onClick={() => setBookableIndex((currentIndex) => (currentIndex + 1) % bookablesInGroup.length)}
+            >Next
+            </button>
+        </div>
+    );
 }
